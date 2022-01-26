754b2fe99a7945f16c9d99715408a07334a4bf16
Gabby <gabriel_marquez@dlsu.edu.ph>
2022-01-26 19:03:57 +0800

    show write-view on click

--- src/main/java/com/example/mysql_client_javafx/HelloController.java
+++ src/main/java/com/example/mysql_client_javafx/HelloController.java
@@ -2,19 +2,30 @@ package com.example.mysql_client_javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+
+import java.io.IOException;
 
 public class HelloController {
     public Button readBtn;
     public Button writeBtn;
 
     @FXML
-    public void onReadBtnClick(ActionEvent actionEvent) {
+    public void onReadBtnClick(ActionEvent actionEvent) throws IOException {
 
     }
-    @FXML
-    public void onWriteBtnClick(ActionEvent actionEvent) {
 
+    @FXML
+    public void onWriteBtnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("write-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Client Application");
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
