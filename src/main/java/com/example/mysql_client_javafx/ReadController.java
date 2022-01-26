7849298973a972bff1f65e6b0ade81da7aeeb50a
Gabby <gabriel_marquez@dlsu.edu.ph>
2022-01-26 19:32:16 +0800

    read view

--- src/main/java/com/example/mysql_client_javafx/ReadController.java
+++ src/main/java/com/example/mysql_client_javafx/ReadController.java
@@ -1,4 +1,27 @@
 package com.example.mysql_client_javafx;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class ReadController {
+    public TextField idInput, titleInput, yearInput;
+    public Text infoTextBtn;
+    public Button getBtn;
+    public Button spamBtn;
+    public Button stopBtn;
+
+    Alert a = new Alert(Alert.AlertType.NONE);
+
+    public void whatsThisInfoPromptClick(MouseEvent mouseEvent) {
+        a = new Alert(Alert.AlertType.INFORMATION);
+        a.setTitle("Information");
+        a.setContentText("You may send consecutive GET requests with the same filter information. "
+        + "This is to monitor data stored on the database for the purpose of testing isolation levels.");
+        a.showAndWait();
+    }
+
+    // TODO: Create a separate window with a TableView container to show the returned GET data.
 }
