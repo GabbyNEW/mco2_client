f421db050891d844075c138e683936c337edf365
Gabby <gabriel_marquez@dlsu.edu.ph>
2022-01-26 19:04:09 +0800

    write view

--- src/main/java/com/example/mysql_client_javafx/WriteController.java
+++ src/main/java/com/example/mysql_client_javafx/WriteController.java
@@ -1,4 +1,24 @@
 package com.example.mysql_client_javafx;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class WriteController {
+    public TextField titleInput, yearInput, rankInput, secondsInput;
+    public Button postBtn;
+    public Text whatsThisClick;
+    public CheckBox doRollbackCheck;
+
+    Alert a = new Alert(Alert.AlertType.NONE);
+
+    public void showSecondsInfoPrompt(MouseEvent mouseEvent) {
+        a = new Alert(Alert.AlertType.INFORMATION);
+        a.setTitle("Information");
+        a.setContentText("Before a transaction is commited by the web server, you may optionally add a timeout before it does so. " +
+                "This is to test isolation levels.");
+        a.showAndWait();
+    }
 }
