e2ada3bd726093611d478196ac67ba083508dd97
Gabby <gabriel_marquez@dlsu.edu.ph>
2022-02-02 12:40:24 +0800

    Add HTTP Requests to read transactions

--- src/main/java/com/example/mysql_client_javafx/ReadController.java
+++ src/main/java/com/example/mysql_client_javafx/ReadController.java
@@ -5,15 +5,46 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.*;
 
 public class ReadController {
     public TextField idInput, titleInput, yearInput;
     public Text infoTextBtn;
-    public Button getBtn;
-    public Button spamBtn;
-    public Button stopBtn;
+    public Button getBtn, spamBtn, stopBtn;
+
+    HttpClient client;
+    HttpRequest request; boolean debug = true;
 
     Alert a = new Alert(Alert.AlertType.NONE);
+
+    public void sendGetRequest() {
+        client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
+        if (debug)
+            request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://206.189.148.74:5001/")) // Replication server
+                    .header("key", "value")
+                    .GET()
+                    .build();
+        else
+            request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://206.189.148.74:5001/")) // Replication server
+                    .header("key", "value")
+                    .GET()
+                    .build();
+        try {
+            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public void whatsThisInfoPromptClick(MouseEvent mouseEvent) {
         a = new Alert(Alert.AlertType.INFORMATION);
